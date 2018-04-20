ef05c6090f0c542aa55ffb69c96eeee45e42722d
robertrayburn <robcrayb@ut.utm.edu>
2018-04-20 12:33:23 -0500

    Added in base project :camel:

---
+++ group-project-angular/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+// ADD THE FIREBASE IMPORTS
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+//
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'angular-group-project-dev'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
